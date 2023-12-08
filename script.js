3e54729ba6ef88020f98fdf441399453c19092a2
vermaaman028 <vermaaman028@gmail.com>
2023-12-07 22:37:54 -0800

    change

--- script.js
+++ script.js
@@ -42,7 +42,7 @@ $(document).ready(function(){
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Full Stack Devloper", "Frontend Devloper", "Backend Devloper", "Freelancer"],
+        strings: ["Software Engineer", "Frontend Devloper", "Freelancer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
